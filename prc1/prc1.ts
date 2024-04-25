f19c4c39817f37fb82d123c5b1ca568692dc1e32
machilan1 <machilan2@gmail.com>
2024-04-26 03:06:22 +0800

    readme

--- prc1/prc1.ts
+++ prc1/prc1.ts
@@ -1,7 +1,12 @@
 // For a script to be executed, it must have a main function that is called at the end of the script.
 const main = () => {
-  console.log("Hello, World!");
+  sayHello();
 };
+
+// You can define functions here
+function sayHello() {
+  console.log("Hello, World!");
+}
 
 // Call the main function
 main();
--- readme.md
+++ readme.md
@@ -1,13 +1,8 @@
-Install globally
-If you want to use tsx anywhere on your computer (without npx), install it globally:
+Install tsx
 
-
-npm
-
-pnpm
-sh
 $ npm install -g tsx
+
+
 Then, you can call tsx directly:
 
-sh
-tsx file.ts
+$ tsx file.ts
